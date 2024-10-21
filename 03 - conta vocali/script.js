82dd5d6623367332777daa517e3c1b6546d185f2
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-21 16:16:47 +0200

    add some changes and output

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -7,17 +7,20 @@ function vowelsCounter(string){
    if(typeof string === 'string'){
     let counter = 0;
         for(let i=0; i<word.length; i++){
-            if(string.charAt(i) === 'a'){
-                console.log('vocale');
+            let char= string.charAt(i);
+            if(char=== 'a'||char=== 'e'||char=== 'i'||char=== 'o'||char=== 'u'){
+                counter ++;
             }
         }
+        return counter;
    }else{
         return 
    }
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-vowelsCounter(word)
+let result = vowelsCounter(word);
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
