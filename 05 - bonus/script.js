410534f244035bdba3e89dab480f0a7380ae4d99
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-21 16:28:08 +0200

    add base function

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,17 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+let greeting = (string)=>{
+    if(typeof string === 'string'){
+        return "ciao "+string;
+    }else{
+        return 
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let result = greeting(name);
+console.log(result);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
