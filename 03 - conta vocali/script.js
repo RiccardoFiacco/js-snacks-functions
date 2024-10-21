7afae7fdbf88ff853558ead7508d952f3e6761bb
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-21 16:14:18 +0200

    add control on input and for to iterate trought the word

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,14 +2,22 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-function vowelsCounter(string, letter){
-
+function vowelsCounter(string){
+   if(typeof string === 'string'){
+    let counter = 0;
+        for(let i=0; i<word.length; i++){
+            if(string.charAt(i) === 'a'){
+                console.log('vocale');
+            }
+        }
+   }else{
+        return 
+   }
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+vowelsCounter(word)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
